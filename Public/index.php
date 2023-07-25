7221fdbdb4f4042fccc79eac4b038157f5c7aede
mariamedev <mariamesouare11@gmail.com>
2023-07-25 15:27:00 +0200

    create htaccess

---
+++ Public/index.php
@@ -0,0 +1,9 @@
+<?php
+//ce fichier est le point d'entrée de notre site
+
+echo " point d'entrée";
+//pour reter sur le fichier index.php quoi qu'il arrive je dois faire une réecriture d'url
+// une des possibilités est d'utiliser un cfuchier de config du serveur apache qui d'appelle 
+// .htaccess
+//nous allos créer ce fichier dans le repertoire "public"
+//*nous allons aussi créer le virtuaHost
