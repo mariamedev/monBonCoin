0b577be0e555c05d4ffc29fafb3300d4af417281
mariamedev <mariamesouare11@gmail.com>
2023-07-26 09:33:22 +0200

    create products controller

--- App/Routeur.php
+++ App/Routeur.php
@@ -2,18 +2,19 @@
 namespace App;
 
 class Routeur{
-    private $routes = [
-        '/' => ['controller' => 'Accueil'], 
-        '/products'=> ['controller' => 'products'],
-        '/detailProduct' => ['controller' => 'detailProduct'],
-        '/incription' => ['controller' => 'incription'],
-        '/connexion' => ['controller' => 'connexion'],
-        '/deconnexion' => ['controller' => 'deconnexion'],
-        '/ajoutProduct' => ['controller' => 'ajoutProduct'],
-        '/modifProduct' => ['controller' => 'modifProduct'],
-        '/suppProduct' => ['controller' => 'suppProduct'],
-        '/panier' => ['controller' => 'panier']  
-     ];
+        private $routes = [
+            '/' => ['controller' => 'Products', 'action' => 'accueil'],
+            '/products' => ['controller' => 'Products', 'action' => 'AffichageProducts'],
+            '/detailProduct' => ['controller' => 'Products', 'action' => 'detailProduct'],
+            '/inscription' => ['controller' => 'Users', 'action' => 'inscription'],
+            '/connexion' => ['controller' => 'Users', 'action' => 'connexion'],
+            '/deconnexion' => ['controller' => 'Users', 'action' => 'deconnexion'],
+            '/ajoutProduct' => ['controller' => 'Products', 'action' => 'ajoutProduct'],
+            '/modifProduct' => ['controller' => 'Products', 'action' => 'modifProduct'],
+            '/suppProduct' => ['controller' => 'Products', 'action' => 'suppProduct'],
+            '/panier' => ['controller' => 'panier', 'action' => 'gestionPanier']
+        ];
+    
     //je creer un methode app qui est le méthode centrale 
     public function app(){
         //on test le routeur
@@ -30,7 +31,10 @@ class Routeur{
 
     // on vérifi si la route ($request) est bien présente dans le tableau de routes
     if (array_key_exists($request, $this->routes)) {
-     echo $this->routes[$request]['controller'];
+        $controller = "Controllers\\" . $this->routes[$request]['controller'];
+        // echo $controller;
+        $action = $this->routes[$request]['action'];
+        $controller::$action();
     }else {
         echo "la page que vous demandez n'existe pas ";
     }
---
+++ Controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Controllers;
+
+class Controller{
+    public static function accueil(){
+        echo "vous êtes dans la méthode accueil";
+        
+    }
+}
